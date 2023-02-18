0a940d67e0f36e5b5535671097a90394980028a2
Julian Bitterlich <julian.bitterlich@soprasteria.com>
2023-02-18 14:07:11 +0100

    change remove target folder

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,14 @@
-module Main exposing (..)
+port module Main exposing (..)
 
 import Browser exposing (Document, application)
+import Element exposing (Attribute, Element)
+import Element.Font
 import Html
 import Html.Attributes
 import Html.Events
+import Json.Decode as D
+import Katex
+import Url
 import Url.Parser
 
 
@@ -16,65 +21,131 @@ main =
         }
 
 
-type Model
-    = Page1
-    | Page2
+type Route
+    = Home
+    | Other
 
 
-init : Model
-init =
-    Page1
+init : String -> ( Route, Cmd Msg )
+init url =
+    ( locationHrefToRoute url, Cmd.none )
 
 
-
---:Model
-
-
-link : msg -> List (Html.Attribute msg) -> List (Html.Html msg) -> Html.Html msg
-link href attrs children =
-    Html.a
-        (Html.Events.preventDefaultOn "click"
-            (D.succeed ( href, True ))
-            :: attrs
-        )
-        children
+type Msg
+    = PushUrl Route
+    | UrlChanged String
 
 
-view : Model -> Document Msg
-view model =
-    { title = "Happy No-Mask-Day"
+update : Msg -> Route -> ( Route, Cmd Msg )
+update msg route =
+    case msg of
+        PushUrl newRoute ->
+            ( route, pushUrl (stringFromRoute newRoute) )
+
+        UrlChanged url ->
+            ( locationHrefToRoute url, renderMath () )
+
+
+stringFromRoute : Route -> String
+stringFromRoute route =
+    case route of
+        Home ->
+            "/"
+
+        Other ->
+            "/other"
+
+
+locationHrefToRoute : String -> Route
+locationHrefToRoute locationHref =
+    case Url.fromString locationHref of
+        Nothing ->
+            Home
+
+        Just url ->
+            Maybe.withDefault Home (Url.Parser.parse routeParser url)
+
+
+routeParser : Url.Parser.Parser (Route -> a) a
+routeParser =
+    Url.Parser.oneOf
+        [ Url.Parser.map Home Url.Parser.top
+        , Url.Parser.map Other (Url.Parser.s "other")
+        ]
+
+
+view : Route -> Document Msg
+view route =
+    { title = "SPA and Katex"
     , body =
-        [ Element.layoutWith
-            { options =
-                [ Element.focusStyle
-                    { borderColor = Nothing
-                    , backgroundColor = Nothing
-                    , shadow = Nothing
-                    }
-                ]
-            }
-            []
-            (pageView model)
+        [ Element.layout
+            [ Element.Font.color (Element.rgb 1 1 1)
+            ]
+            (Element.el
+                [ Element.centerX, Element.centerY ]
+                (viewPage route)
+            )
         ]
     }
 
 
-pageView : Model -> Element Msg
-pageView model =
-    case model.route of
-        Just Home ->
-            Pages.Home.pageView
+viewPage : Route -> Element Msg
+viewPage route =
+    case route of
+        Home ->
+            Element.column [ Element.spacing 20 ]
+                [ link Other "Link to Other"
+                , Element.text "some text"
+                , "\\mathrm{home} =  6.2 \\times 10^{-34}"
+                    |> Katex.inline
+                    |> Katex.print
+                    |> Element.text
+                ]
 
-        Just DefinitonOfNoMaskDay ->
-            Pages.WhatIsNoMaskDay.view
+        Other ->
+            Element.column [ Element.spacing 20 ]
+                [ link Home "Link to Home"
+                , "\\mathrm{other} = 1.3 \\times 10^{-6}"
+                    |> Katex.inline
+                    |> Katex.print
+                    |> Element.text
+                , Element.text "other text"
+                ]
 
-        Just LowPoints ->
-            Pages.LowPoints.view
 
-        Nothing ->
-            [ text "route=nothing" ]
-                |> PageLayouts.homePageLayout Element.none "bla"
+linkBehaviour : Route -> Attribute Msg
+linkBehaviour route =
+    Element.htmlAttribute
+        (Html.Events.preventDefaultOn "click"
+            (D.succeed
+                ( PushUrl route, True )
+            )
+        )
 
 
+link : Route -> String -> Element Msg
+link route labelText =
+    Element.link
+        [ linkBehaviour route
+        , Element.Font.color (Element.rgb255 119 35 177)
+        , Element.Font.underline
+        ]
+        { url = stringFromRoute route
+        , label = Element.text labelText
+        }
 
--- Ui-Elements
+
+subscriptions : Route -> Sub Msg
+subscriptions route =
+    Sub.batch
+        [ onUrlChange UrlChanged
+        ]
+
+
+port onUrlChange : (String -> msg) -> Sub msg
+
+
+port pushUrl : String -> Cmd msg
+
+
+port renderMath : () -> Cmd msg
